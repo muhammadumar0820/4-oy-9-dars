8e521febb8830d7967b574cde9bd489960b0d615
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-26 17:38:07 +0300

    masalalar bajarildi

---
+++ index.js
@@ -0,0 +1,33 @@
+//Birinchi masala
+//let arr = [5, 3 , 6, -4, 2, 7, -1];
+//let arr2 = arr.filter((value, index, arr) =>{
+//    return value > 5;
+//});
+//console.log(arr2);
+
+//Ikkinchi masala
+// const products = [
+//   { id: 1, name: 'compter', brand: 'Lenovo', price: 1324.99 },
+//   { id: 2, name: 'Smartwatch', brand: 'Iphone', price: 9999.99 },
+//   { id: 3, name: 'phone', brand: 'Android', price: 349.99 },
+//   { id: 4, name: 'ball', brand: 'Jordan', price: 197.95 },
+// ];
+// console.log(filterProducts(products, 'brand', 'Iphone'));
+
+//Uchinchi masala
+
+
+//To'rtinchi masala
+// const tasks = [
+//      { Name: 'task A', priority: 'Low', deadline: '2023-05-03' },
+//      { Name: 'task B', priority: 'Medium', deadline: '2023-07-12' },
+//      { Name: 'task C', priority: 'High', deadline: '2023-07-21' },
+//      { Name: 'task D', priority: 'Medium', deadline: '2023-08-11' },
+//    ];
+// sortTasksByDeadline(tasks);
+// console.log(tasks)
+
+//Beshinchi masala
+//let arr = [2, 3, 4, 5, 6, 7, 8];
+//let arr1 = arr.slice(1, 5);
+//console.log(arr1);
